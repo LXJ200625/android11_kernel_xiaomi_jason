90618bef798d4120ffe16e6a87ef0bddf0d85426
LXJ200625 <744099293@qq.com>
2026-02-21 14:04:16 +0800

    Add files via upload

--- fs/exec.c
+++ fs/exec.c
@@ -1533,9 +1533,6 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return ret;
 }
 
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
-			       void *envp, int *flags);
-
 /*
  * sys_execve() executes a new program.
  */
@@ -1549,8 +1546,6 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct file *file;
 	struct files_struct *displaced;
 	int retval;
-
-	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1685,12 +1680,23 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
+
 int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
+
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+
+#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit ksud and 32-on-64 support
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif	
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
